c193c5fc8b54bbb79900d8fd7396efefa0d046f9
lance-o <cilialawrence@yahoo.co.uk>
2024-05-24 13:34:35 +0100

    love git so much

---
+++ config/custom-formats.ts
@@ -0,0 +1,17 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+	{
+		section: "Gen 9 Pet Mods",
+		column: 1,
+		// name: "gen9petmods",
+	},
+	{
+		name: "[Gen 3] Betterald",
+		desc: ["<b>Betterald</b>: Beachcube's favourite."],
+		mod: 'betterald',
+		banlist: ['Uber'],
+		unbanlist: ['Sand Veil',],
+	},
+];
