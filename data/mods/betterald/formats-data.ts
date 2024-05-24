a48e930e7259a95344b3cf8b6ec5b0d4534b655f
lance-o <cilialawrence@yahoo.co.uk>
2024-05-24 14:37:09 +0100

    add tiers

--- data/mods/betterald/formats-data.ts
+++ data/mods/betterald/formats-data.ts
@@ -1175,4 +1175,607 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	deoxysspeed: {
 		tier: "Uber",
 	},
+	adamantris: {
+    tier: "OU",
+ },
+aetherpent: {
+    tier: "OU",
+ },
+akopi: {
+    tier: "OU",
+ },
+ameguano: {
+    tier: "OU",
+ },
+ammunick: {
+    tier: "OU",
+ },
+ankyron: {
+    tier: "OU",
+ },
+arphillery: {
+    tier: "OU",
+ },
+articfly: {
+    tier: "OU",
+ },
+asterf: {
+    tier: "OU",
+ },
+babybolt: {
+    tier: "OU",
+ },
+battlenose: {
+    tier: "OU",
+ },
+beachcube: {
+    tier: "OU",
+ },
+bigbrue: {
+    tier: "OU",
+ },
+bigshaq: {
+    tier: "OU",
+ },
+binglebop: {
+    tier: "OU",
+ },
+bizaare: {
+    tier: "OU",
+ },
+blording: {
+    tier: "OU",
+ },
+boltzy: {
+    tier: "OU",
+ },
+boodle: {
+    tier: "OU",
+ },
+borb: {
+    tier: "OU",
+ },
+bostfright: {
+    tier: "OU",
+ },
+brackedeer: {
+    tier: "OU",
+ },
+burowurm: {
+    tier: "OU",
+ },
+buuny: {
+    tier: "OU",
+ },
+cadirt: {
+    tier: "OU",
+ },
+calibert: {
+    tier: "OU",
+ },
+canishade: {
+    tier: "OU",
+ },
+cascuffle: {
+    tier: "OU",
+ },
+caterchill: {
+    tier: "OU",
+ },
+celesdee: {
+    tier: "OU",
+ },
+centerrayn: {
+    tier: "OU",
+ },
+chillcoon: {
+    tier: "OU",
+ },
+chinocido: {
+    tier: "OU",
+ },
+chirpeak: {
+    tier: "OU",
+ },
+chokaze: {
+    tier: "OU",
+ },
+chronorf: {
+    tier: "OU",
+ },
+chubb: {
+    tier: "OU",
+ },
+clevenge: {
+    tier: "OU",
+ },
+clifhead: {
+    tier: "OU",
+ },
+clodd: {
+    tier: "OU",
+ },
+cloriff: {
+    tier: "OU",
+ },
+coroverine: {
+    tier: "OU",
+ },
+cragnet: {
+    tier: "OU",
+ },
+crashilisk: {
+    tier: "OU",
+ },
+crewnium: {
+    tier: "OU",
+ },
+dandyflyon: {
+    tier: "OU",
+ },
+dandylion: {
+    tier: "OU",
+ },
+darudape: {
+    tier: "OU",
+ },
+deludent: {
+    tier: "OU",
+ },
+diaguano: {
+    tier: "OU",
+ },
+dimensean: {
+    tier: "OU",
+ },
+dolorgeist: {
+    tier: "OU",
+ },
+drildo: {
+    tier: "OU",
+ },
+ducksu: {
+    tier: "OU",
+ },
+ecstatick: {
+    tier: "OU",
+ },
+einzarm: {
+    tier: "OU",
+ },
+eleckton: {
+    tier: "OU",
+ },
+eliminape: {
+    tier: "OU",
+ },
+espuahua: {
+    tier: "OU",
+ },
+etchead: {
+    tier: "OU",
+ },
+fixadent: {
+    tier: "OU",
+ },
+flamberd: {
+    tier: "OU",
+ },
+flegs: {
+    tier: "OU",
+ },
+flody: {
+    tier: "OU",
+ },
+flowpiary: {
+    tier: "OU",
+ },
+fuugolf: {
+    tier: "OU",
+ },
+gauntlant: {
+    tier: "OU",
+ },
+gecc: {
+    tier: "OU",
+ },
+geometrick: {
+    tier: "OU",
+ },
+glassilisk: {
+    tier: "OU",
+ },
+golinus: {
+    tier: "OU",
+ },
+goofbass: {
+    tier: "OU",
+ },
+goosgue: {
+    tier: "OU",
+ },
+gopnik: {
+    tier: "OU",
+ },
+gregorlon: {
+    tier: "OU",
+ },
+grimodeus: {
+    tier: "OU",
+ },
+grimp: {
+    tier: "OU",
+ },
+groach: {
+    tier: "OU",
+ },
+gyucc: {
+    tier: "OU",
+ },
+hachifox: {
+    tier: "OU",
+ },
+hallustial: {
+    tier: "OU",
+ },
+hamastar: {
+    tier: "OU",
+ },
+hamatonic: {
+    tier: "OU",
+ },
+helpmee: {
+    tier: "OU",
+ },
+hemaskeeto: {
+    tier: "OU",
+ },
+hindenboom: {
+    tier: "OU",
+ },
+hobshark: {
+    tier: "OU",
+ },
+honkhonk: {
+    tier: "OU",
+ },
+hoppily: {
+    tier: "OU",
+ },
+ichifox: {
+    tier: "OU",
+ },
+ike: {
+    tier: "OU",
+ },
+impactaur: {
+    tier: "OU",
+ },
+intesterra: {
+    tier: "OU",
+ },
+jostrich: {
+    tier: "OU",
+ },
+kaber: {
+    tier: "OU",
+ },
+kaens: {
+    tier: "OU",
+ },
+kamizao: {
+    tier: "OU",
+ },
+kapter: {
+    tier: "OU",
+ },
+kaugurise: {
+    tier: "OU",
+ },
+klaximus: {
+    tier: "OU",
+ },
+kwismet: {
+    tier: "OU",
+ },
+lahgoon: {
+    tier: "OU",
+ },
+larcenyzel: {
+    tier: "OU",
+ },
+leadaghoul: {
+    tier: "OU",
+ },
+lepsisand: {
+    tier: "OU",
+ },
+longcow: {
+    tier: "OU",
+ },
+lunear: {
+    tier: "OU",
+ },
+marth: {
+    tier: "OU",
+ },
+mcbaphomet: {
+    tier: "OU",
+ },
+mhantom: {
+    tier: "OU",
+ },
+monkrazy: {
+    tier: "OU",
+ },
+mrkrabs: {
+    tier: "OU",
+ },
+mubbius: {
+    tier: "OU",
+ },
+mudbisco: {
+    tier: "OU",
+ },
+mudisco: {
+    tier: "OU",
+ },
+muscleaf: {
+    tier: "OU",
+ },
+narpasord: {
+    tier: "OU",
+ },
+neuroscale: {
+    tier: "OU",
+ },
+noxiousmon: {
+    tier: "OU",
+ },
+nuggy: {
+    tier: "OU",
+ },
+omnibola: {
+    tier: "OU",
+ },
+paddetoad: {
+    tier: "OU",
+ },
+parashale: {
+    tier: "OU",
+ },
+pawnr: {
+    tier: "OU",
+ },
+peacreaper: {
+    tier: "OU",
+ },
+pengone: {
+    tier: "OU",
+ },
+pinkgo: {
+    tier: "OU",
+ },
+planktron: {
+    tier: "OU",
+ },
+plommet: {
+    tier: "OU",
+ },
+pluspectre: {
+    tier: "OU",
+ },
+poisonmon: {
+    tier: "OU",
+ },
+polcamo: {
+    tier: "OU",
+ },
+polevolt: {
+    tier: "OU",
+ },
+poundsauce: {
+    tier: "OU",
+ },
+pyratinom: {
+    tier: "OU",
+ },
+quadruffle: {
+    tier: "OU",
+ },
+quarophus: {
+    tier: "OU",
+ },
+quostrich: {
+    tier: "OU",
+ },
+rabboom: {
+    tier: "OU",
+ },
+radillo: {
+    tier: "OU",
+ },
+rainwood: {
+    tier: "OU",
+ },
+rambull: {
+    tier: "OU",
+ },
+ratomic: {
+    tier: "OU",
+ },
+recyclimp: {
+    tier: "OU",
+ },
+retrover: {
+    tier: "OU",
+ },
+roguano: {
+    tier: "OU",
+ },
+rookraw: {
+    tier: "OU",
+ },
+runguy: {
+    tier: "OU",
+ },
+salmeal: {
+    tier: "OU",
+ },
+sapleak: {
+    tier: "OU",
+ },
+saviathan: {
+    tier: "OU",
+ },
+seedos: {
+    tier: "OU",
+ },
+seedotbetterald: {
+    tier: "OU",
+ },
+seetres: {
+    tier: "OU",
+ },
+shifox: {
+    tier: "OU",
+ },
+shiroblank: {
+    tier: "OU",
+ },
+silily: {
+    tier: "OU",
+ },
+silverine: {
+    tier: "OU",
+ },
+sinistall: {
+    tier: "OU",
+ },
+sleuther: {
+    tier: "OU",
+ },
+smokaze: {
+    tier: "OU",
+ },
+smooke: {
+    tier: "OU",
+ },
+snoogle: {
+    tier: "OU",
+ },
+snowst: {
+    tier: "OU",
+ },
+solear: {
+    tier: "OU",
+ },
+soulogic: {
+    tier: "OU",
+ },
+spharaoh: {
+    tier: "OU",
+ },
+sphempress: {
+    tier: "OU",
+ },
+squalash: {
+    tier: "OU",
+ },
+stalagnet: {
+    tier: "OU",
+ },
+stohmatic: {
+    tier: "OU",
+ },
+stomm: {
+    tier: "OU",
+ },
+stumpole: {
+    tier: "OU",
+ },
+sykomantis: {
+    tier: "OU",
+ },
+tackalf: {
+    tier: "OU",
+ },
+thestorm: {
+    tier: "OU",
+ },
+thonkey: {
+    tier: "OU",
+ },
+thriitake: {
+    tier: "OU",
+ },
+tikid: {
+    tier: "OU",
+ },
+tikindle: {
+    tier: "OU",
+ },
+tikinferno: {
+    tier: "OU",
+ },
+tinychu: {
+    tier: "OU",
+ },
+tooshroom: {
+    tier: "OU",
+ },
+toxicmon: {
+    tier: "OU",
+ },
+tumbling: {
+    tier: "OU",
+ },
+tussleweed: {
+    tier: "OU",
+ },
+twerkyvats: {
+    tier: "OU",
+ },
+vacaguin: {
+    tier: "OU",
+ },
+vampunit: {
+    tier: "OU",
+ },
+vampuny: {
+    tier: "OU",
+ },
+vamsheol: {
+    tier: "OU",
+ },
+voland: {
+    tier: "OU",
+ },
+warnhal: {
+    tier: "OU",
+ },
+weakleaf: {
+    tier: "OU",
+ },
+weaksel: {
+    tier: "OU",
+ },
+wheelo: {
+    tier: "OU",
+ },
+worland: {
+    tier: "OU",
+ },
+wrathilisk: {
+    tier: "OU",
+ },
+wumpydump: {
+    tier: "OU",
+ },
+yediggit: {
+    tier: "OU",
+ },
+zungarm: {
+    tier: "OU",
+ },
 };
