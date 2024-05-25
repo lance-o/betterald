f2e76a1e8d7670bac2b21edc236bdd0069c80073
lance-o <cilialawrence@yahoo.co.uk>
2024-05-25 01:00:28 +0100

    awful codebase

--- data/mods/betterald2/rulesets.ts
+++ data/mods/betterald2/rulesets.ts
@@ -5,19 +5,12 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		desc: "The standard ruleset for all offical Smogon singles tiers (Ubers, OU, etc.)",
 		ruleset: ['Obtainable', 'Sleep Clause Mod', 'Switch Priority Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Evasion Items Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
 	},
-	betterald: {
+	betteraldruleset: {
 		effectType: 'ValidatorRule',
 		name: 'Betterald Ruleset',
 		desc: "Works pls",
 		ruleset: [
 			'Obtainable', '+Unobtainable', '+Past', 'Sketch Post-Gen 7 Moves', 'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause',
 		],
-		onValidateSet(set) {
-			const species = this.dex.species.get(set.species);
-			if (species.tier != 'Betterald OU') {
-				if (this.ruleTable.has(`+pokemon:${species.id}`)) return;
-				return [`${set.name || set.species} does not exist in the National Dex.`];
-			}
-		},
 	},
 };
