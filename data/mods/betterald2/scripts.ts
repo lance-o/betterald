6997e2a8edb39b93b31d5b09535b3c8e3171f354
lance-o <cilialawrence@yahoo.co.uk>
2024-05-31 23:24:00 +0100

    update moves

--- data/mods/betterald2/scripts.ts
+++ data/mods/betterald2/scripts.ts
@@ -1,13 +1,8 @@
-import { movesUpdate } from "../../mods/betterald2/movesUpdate";
-
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen4',
 	gen: 3,
 	teambuilderConfig: {
 		excludeStandardTiers: true,
 		customTiers: ['B-OU'],
-	},
-	init() {
-		movesUpdate(this);
 	},
 };
