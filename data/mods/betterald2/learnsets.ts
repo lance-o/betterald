d59124ce23448786e6523d9a09d7bd64ea546331
lance-o <cilialawrence@yahoo.co.uk>
2024-06-01 21:18:23 +0100

    give everyone sketch for testing

--- data/mods/betterald2/learnsets.ts
+++ data/mods/betterald2/learnsets.ts
@@ -6,1012 +6,1008 @@ export const Learnsets: {[speciesid: string]: ModdedLearnsetData} = {
 	// 3T = gen 3, tutor.
 	adamantris: {
    learnset: {
-			tackle: ["3L1"],
-			bugbite: ["3L1"],
-			takedown: ["3L1"],
-			shadowclaw: ["3L1"],
 			sketch: ["3L1"],
 
    },
  },
 aetherpent: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 akopi: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ameguano: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ammunick: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ankyron: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 arphillery: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 articfly: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 asterf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 babybolt: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 battlenose: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 beachcube: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 bigbrue: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 bigshaq: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 binglebop: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 bizaare: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 blording: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 boltzy: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 boodle: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 borb: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 bostfright: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 brackedeer: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 burowurm: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 buuny: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 cadirt: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 calibert: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 canishade: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 cascuffle: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 caterchill: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 celesdee: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 centerrayn: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chillcoon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chinocido: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chirpeak: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chokaze: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chronorf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 chubb: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 clevenge: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 clifhead: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 clodd: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 cloriff: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 coroverine: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 cragnet: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 crashilisk: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 crewnium: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 dandyflyon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 dandylion: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 darudape: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 deludent: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 diaguano: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 dimensean: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 dolorgeist: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 drildo: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ducksu: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ecstatick: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 einzarm: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 eleckton: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 eliminape: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 espuahua: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 etchead: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 fixadent: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 flamberd: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 flegs: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 flody: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 flowpiary: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 fuugolf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 gauntlant: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 gecc: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 geometrick: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 glassilisk: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 golinus: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 goofbass: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 goosgue: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 gopnik: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 gregorlon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 grimodeus: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 grimp: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 groach: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 gyucc: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hachifox: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hallustial: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hamastar: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hamatonic: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 helpmee: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hemaskeeto: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hindenboom: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hobshark: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 honkhonk: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 hoppily: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ichifox: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ike: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 impactaur: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 intesterra: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 jostrich: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kaber: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kaens: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kamizao: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kapter: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kaugurise: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 klaximus: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 kwismet: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 lahgoon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 larcenyzel: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 leadaghoul: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 lepsisand: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 longcow: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 lunear: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 marth: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mcbaphomet: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mhantom: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 monkrazy: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mrkrabs: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mubbius: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mudbisco: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 mudisco: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 muscleaf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 narpasord: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 neuroscale: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 noxiousmon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 nuggy: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 omnibola: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 paddetoad: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 parashale: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 pawnr: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 peacreaper: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 pengone: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 pinkgo: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 planktron: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 plommet: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 pluspectre: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 poisonmon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 polcamo: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 polevolt: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 poundsauce: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 pyratinom: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 quadruffle: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 quarophus: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 quostrich: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 rabboom: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 radillo: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 rainwood: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 rambull: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 ratomic: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 recyclimp: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 retrover: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 roguano: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 rookraw: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 runguy: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 salmeal: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 sapleak: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 saviathan: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 seedos: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 seedotbetterald: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 seetres: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 shifox: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 shiroblank: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 silily: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 silverine: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 sinistall: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 sleuther: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 smokaze: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 smooke: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 snoogle: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 snowst: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 solear: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 soulogic: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 spharaoh: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 sphempress: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 squalash: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 stalagnet: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 stohmatic: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 stomm: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 stumpole: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 sykomantis: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tackalf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 thestorm: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 thonkey: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 thriitake: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tikid: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tikindle: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tikinferno: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tinychu: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tooshroom: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 toxicmon: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tumbling: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 tussleweed: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 twerkyvats: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 vacaguin: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 vampunit: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 vampuny: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 vamsheol: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 voland: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 warnhal: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 weakleaf: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 weaksel: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 wheelo: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 worland: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 wrathilisk: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 wumpydump: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 yediggit: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 zungarm: {
    learnset: {
-   tackle: ["3L1"],
+   sketch: ["3L1"],
    },
  },
 };
